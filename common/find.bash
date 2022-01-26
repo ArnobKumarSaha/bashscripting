e25310060de45d9f8b2e792b636bee0cc3eae74a
Arnob kumar saha <arnob@appscode.com>
2022-01-26 19:27:45 +0600

    find command from corey schafer

---
+++ common/find.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# AUTHOR : Arnob kumar saha
+# Date created : 26-01-2022
+# Lase modified : 26-01-2022
+
+# DESCRIPTION : This script will show some stuffs that can be done using find.
+# USAGE : ./find.bash
+
+find ./demo  # find everything (multilevel) in demo directory
+# to set the depth of level, use
+-maxdepth <depth>
+
+-type d # only directories
+-type f # only files
+
+-name <a-name> # wildcard supported
+-iname <a-name> # case insensitive use of '-name'
+
+# m for modified, min for minute
+-mmin -10 # those are modified less than 10 minutes ago
+-mmin +10 # those are modified more than 10 minutes ago
+-mtime # same as above, just calculation goes on days instead of minutes
+# a for access -> amin, atime
+# c dor chance -> cmin, ctime
+
+-size +5M # size more than 5 megabytes, K for kiloBytes, G for gigaBytes
+-empty # find all files those are empty (containes nothing)
+-perm 755 # those have permission 755
+
+find demo -exec chown arnob:some-group {} + # executing `chown arnob:some-group <file-name>` commnad
+# {} is just a placeholder for file-name,  And + is the sign to end the exec command. 
+# \; could be used instead of +  and any sign could be used as placeholder
