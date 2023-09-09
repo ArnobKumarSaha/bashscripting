8df1340d98abd30d86f43ab23ca1faa9af5f4330
Arnob kumar saha <arnob@appscode.com>
2023-09-09 23:24:27 +0600

    learning find
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

--- common/find.bash
+++ common/find.bash
@@ -2,7 +2,7 @@
 
 # AUTHOR : Arnob kumar saha
 # Date created : 26-01-2022
-# Lase modified : 26-01-2022
+# Lase modified : 05-09-2023
 
 # DESCRIPTION : This script will show some stuffs that can be done using find.
 # USAGE : ./find.bash
@@ -15,7 +15,7 @@ find ./demo  # find everything (multilevel) in demo directory
 -type f # only files
 
 -name <a-name> # wildcard supported
--iname <a-name> # case insensitive use of '-name'
+-iname <a-name> # case insensitive use of '-name' 
 
 # m for modified, min for minute
 -mmin -10 # those are modified less than 10 minutes ago
@@ -31,3 +31,11 @@ find ./demo  # find everything (multilevel) in demo directory
 find demo -exec chown arnob:some-group {} + # executing `chown arnob:some-group <file-name>` commnad
 # {} is just a placeholder for file-name,  And + is the sign to end the exec command. 
 # \; could be used instead of +  and any sign could be used as placeholder
+
+find . -path */store/*  # Find the paths that match */store/*
+find . -iname *.sh -o -name *.bash  # all shell or bash files
+find . ! -name p*.bash   # not operator
+
+# to find the readme files with path */doc/*, or license files with arbitrary paths
+find / \( -path */doc/* -name README \) -o -name LICENSE  # backslashes to escape
+
